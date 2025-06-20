935a2f45f3c33ee9c4d79075d45152b0f9623bd9
TensorCruncher <tensorcruncher@gmail.com>
2025-06-20 08:33:48 +0530

    Moved data.py, added functions to it

---
+++ utils/data.py
@@ -0,0 +1,128 @@
+import torch
+from torch import nn
+
+import torchvision
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+import os
+import requests
+from pathlib import Path
+import zipfile
+
+NUM_WORKERS = os.cpu_count()
+
+def create_dataloaders(
+    train_dir: str, 
+    test_dir: str, 
+    transform: transforms.Compose, 
+    batch_size: int, 
+    num_workers: int=NUM_WORKERS
+):
+  """Creates training and testing DataLoaders.
+  
+  Args:
+    train_dir: Path to training directory.
+    test_dir: Path to testing directory.
+    transform: torchvision transforms to perform on training and testing data.
+    batch_size: Number of samples per batch in each of the DataLoaders.
+    num_workers: An integer for number of workers per DataLoader.
+
+  Returns:
+    A tuple of (train_dataloader, test_dataloader, class_names).
+    Where class_names is a list of the target classes.
+  """
+  # Use ImageFolder to create dataset(s)
+  train_data = datasets.ImageFolder(train_dir, transform=transform)
+  test_data = datasets.ImageFolder(test_dir, transform=transform)
+
+  # Get class names
+  class_names = train_data.classes
+
+  # Turn images into data loaders
+  train_dataloader = DataLoader(
+      train_data,
+      batch_size=batch_size,
+      shuffle=True,
+      num_workers=num_workers,
+      pin_memory=True,
+  )
+  test_dataloader = DataLoader(
+      test_data,
+      batch_size=batch_size,
+      shuffle=False,
+      num_workers=num_workers,
+      pin_memory=True,
+  )
+
+  return train_dataloader, test_dataloader, class_names
+
+def download_data(source: str, 
+                  destination: str,
+                  remove_source: bool = True):
+    """Downloads a zipped dataset from source and unzips to destination.
+
+    Args:
+        source (str): A link to a zipped file containing data.
+        destination (str): A target directory to unzip data to.
+        remove_source (bool): Whether to remove the source after downloading and extracting.
+    
+    Returns:
+        pathlib.Path to downloaded data.
+    """
+    # Setup path to data folder
+    data_path = Path("data/")
+    image_path = data_path / destination
+
+    # If the image folder doesn't exist, download it and prepare it... 
+    if image_path.is_dir():
+        print(f"[INFO] {image_path} directory exists, skipping download.")
+    else:
+        print(f"[INFO] Did not find {image_path} directory, creating one...")
+        image_path.mkdir(parents=True, exist_ok=True)
+        
+        # Download pizza, samosa, tacos data
+        target_file = Path(source).name
+        with open(data_path / target_file, "wb") as f:
+            request = requests.get(source)
+            print(f"[INFO] Downloading {target_file} from {source}...")
+            f.write(request.content)
+
+        # Unzip pizza, steak, sushi data
+        with zipfile.ZipFile(data_path / target_file, "r") as zip_ref:
+            print(f"[INFO] Unzipping {target_file} data...") 
+            zip_ref.extractall(image_path)
+
+        # Remove .zip file
+        if remove_source:
+            os.remove(data_path / target_file)
+    
+    return image_path
+
+def split_dataset(dataset:torchvision.datasets, split_size:float=0.2, seed:int=42):
+    """Randomly splits a given dataset into two proportions based on split_size and seed.
+
+    Args:
+        dataset (torchvision.datasets): A PyTorch Dataset.
+        split_size (float, optional): How much of the dataset should be split? 
+        seed (int, optional): Seed for random generator. Defaults to 42.
+
+    Returns:
+        tuple: (random_split_1, random_split_2) where random_split_1 is of size split_size*len(dataset) and 
+            random_split_2 is of size (1-split_size)*len(dataset).
+    """
+    # Create split lengths based on original dataset length
+    length_1 = int(len(dataset) * split_size) # desired length
+    length_2 = len(dataset) - length_1 # remaining length
+        
+    # Print out info
+    print(f"[INFO] Splitting dataset of length {len(dataset)} into splits of size: {length_1} ({int(split_size*100)}%), {length_2} ({int((1-split_size)*100)}%)")
+    
+    # Create splits with given random seed
+    random_split_1, random_split_2 = torch.utils.data.random_split(dataset, 
+                                                                   lengths=[length_1, length_2],
+                                                                   generator=torch.manual_seed(seed))
+    return random_split_1, random_split_2
