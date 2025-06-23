3d9e05beb02ba61f2bb812877b1d2856fb830f6c
TensorCruncher <tensorcruncher@gmail.com>
2025-06-23 19:16:31 +0530

    Added helper function for setting seeds

--- utils/misc.py
+++ utils/misc.py
@@ -25,3 +25,12 @@ def save_model(model: torch.nn.Module,
     print(f"[INFO] Saving model to: {model_save_path}")
     torch.save(obj=model.state_dict(),
              f=model_save_path)
+
+def set_seeds(seed: int=42):
+    """Sets random sets for torch operations.
+
+    Args:
+        seed (int, optional): Random seed to set. Defaults to 42.
+    """
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
